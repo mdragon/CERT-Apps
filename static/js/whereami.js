ea54a81c41051b7fae1a22502d1406258f72e40f
Matt Dragon <matt.dragon@jhu.edu>
2015-04-11 20:54:43 -0400

    Better error handling on the client and server

--- static/js/whereami.js
+++ static/js/whereami.js
@@ -7,10 +7,27 @@ var form = document.forms["data"];
 var name = form["name"];
 var lat = form["lat"];
 var lon = form["long"];
+var updated = form["updated"];
 var submit = form["submit"];
 
 var init = function() {
 	submit.disabled = true;
+};
+
+var getPrettyDate = function(date) {
+	date = date || new Date();
+	var hour = date.getHours().toString();
+	if( hour.length == 1 ) hour = "0" + hour;
+	
+	var minute = date.getMinutes().toString();
+	if( minute.length == 1 ) minute = "0" + minute;
+
+	var second = date.getSeconds().toString();
+	if( second.length == 1 ) second = "0" + second;
+
+	var strTime = hour + ":" + minute + ":" + second;
+
+	return strTime;
 };
 
 var updateLocation = function() {
@@ -45,13 +62,34 @@ var updateLocation = function() {
 			lonSpan.innerHTML = stringLong.substring(0,len);
 
 			var date = new Date();
-			strTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+			strTime = getPrettyDate(date);
 
 			atSpan.innerHTML = "as of " + strTime;
+
+			updated.value = date.toISOString();// + "00:00";
 
 			submit.disabled = false;
 
 			console.groupEnd()
+		}, function(error) {
+			var errMsg = null;
+
+			switch(error.code)
+			{
+				case 0: 
+					errMsg = "Unknown Error";
+					break;
+				case 1: 
+					errMsg = "Permission denied, please refresh the page and/or \"Allow\" the Location permission";
+					break;
+				case 2: 
+					errMsg = "Location unavailble, try again";
+					breal;
+				case 3: 
+					errMsg = "Location request timed out, try again.";
+			}
+
+			errorSpan.innerText = "Geolocation error: " + errMsg;
 		});
 	} else {
 		console.error('Geolocation is not supported!');
