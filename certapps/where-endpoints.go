a8c173c6144857b40ba2a1b0ccf6a4599f5f8eab
Matt Dragon <mdragon@charitynavigator.org>
2016-03-29 18:27:13 -0400

    WIP OAuth in Endpoints

--- certapps/where-endpoints.go
+++ certapps/where-endpoints.go
@@ -39,3 +39,26 @@ func (gs *WhereAmIService) List(c endpoints.Context, r *WhereAmIListReq) (*Where
 
 	return &WhereAmIList{wheres}, nil
 }
+
+// List responds with a list of all greetings ordered by Date field.
+// Most recent greets come first.
+func (gs *WhereAmIService) ListOAuth(c endpoints.Context, r *WhereAmIListReq) (*WhereAmIList, error) {
+	if r.Limit <= 0 {
+		r.Limit = 500
+	}
+
+	q := datastore.NewQuery("WhereAmI").Order("-Entered").Limit(r.Limit)
+	wheres := make([]*WhereAmI, 0, r.Limit)
+	keys, err := q.GetAll(c, &wheres)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, k := range keys {
+		w := wheres[i]
+		w.Key = k
+		w.KeyID = k.IntID()
+	}
+
+	return &WhereAmIList{wheres}, nil
+}
